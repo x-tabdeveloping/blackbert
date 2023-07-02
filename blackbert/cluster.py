fca4da0c82bd66d9136dfedf8b363a391c31a570
Márton Kardos <power.up1163@gmail.com>
2023-07-02 19:39:08 +0200

    Added convenience functions to mixtures and clustering algorithms.

--- blackbert/cluster.py
+++ blackbert/cluster.py
@@ -1,15 +1,56 @@
 from sklearn.base import ClusterMixin, TransformerMixin
+from sklearn.cluster import DBSCAN, KMeans, SpectralClustering
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import LabelBinarizer
 
 
 class ClusterTransformer(TransformerMixin):
-    def __init__(self, model: ClusterMixin, n_neighbors: int = 5):
+    """Turns sklearn clustering algorithm into a transformer component that
+    you can use in a pipeline.
+    If the clustering method cannot be used for prediction
+    (aka. it does not have a predict method), then a nearest neighbour
+    vot will be used to infer cluster labels for unseen samples.
+
+    Parameters
+    ----------
+    model: ClusterMixin
+        Sklearn clustering model.
+    n_neighbors: int
+        Number of neighbours to use for inference.
+    metric: str
+        Metric to use for determining nearest neighbours.
+
+    Attributes
+    ----------
+    labeler: LabelBinarizer
+        Component that turns cluster labels into one-hot embeddings.
+    neighbors: KNeighborsClassifier
+        Classifier to use for out of sample prediction.
+    """
+
+    def __init__(
+        self, model: ClusterMixin, n_neighbors: int = 5, metric: str = "cosine"
+    ):
         self.model = model
         self.labeler = LabelBinarizer()
-        self.neighbors = KNeighborsClassifier(n_neighbors=n_neighbors)
+        self.neighbors = KNeighborsClassifier(
+            n_neighbors=n_neighbors, metric=metric
+        )
 
     def fit(self, X, y=None):
+        """Fits the clustering algorithm and label binarizer.
+
+        Parameters
+        ----------
+        X: ndarray of shape (n_observations, n_features)
+            Observations to cluster.
+        y: None
+            Ignored, exists for compatiblity.
+
+        Returns
+        -------
+        self
+        """
         labels = self.model.fit_predict(X)
         if not hasattr(self.model, "predict"):
             self.neighbors.fit(X, labels)
@@ -17,8 +58,95 @@ class ClusterTransformer(TransformerMixin):
         return self
 
     def transform(self, X):
+        """Infers cluster labels for given data points.
+
+        Parameters
+        ----------
+        X: ndarray of shape (n_observations, n_features)
+            Observations to cluster.
+
+        Returns
+        -------
+        ndarray of shape (n_observations, n_clusters)
+            One-hot encoding of cluster labels.
+        """
         if hasattr(self.model, "predict"):
             labels = self.model.predict(X)
         else:
             labels = self.neighbors.predict(X)
         return self.labeler.transform(labels)
+
+    def get_feature_names_out(self):
+        """Returns the cluster classes for each dimension.
+
+        Returns
+        -------
+        ndarray of shape (n_clusters)
+            Cluster names.
+        """
+        return self.labeler.classes_
+
+
+def DBSCANTransformer(
+    eps: float = 0.5, min_samples: int = 5, metric: str = "cosine"
+) -> ClusterTransformer:
+    """Convenience function for creating a DBSCAN transformer.
+
+    Parameters
+    ----------
+    eps : float, default 0.5
+        The maximum distance between two samples for one to be considered
+        as in the neighborhood of the other. This is not a maximum bound
+        on the distances of points within a cluster. This is the most
+        important DBSCAN parameter to choose appropriately for your data set
+        and distance function.
+
+    min_samples : int, default 5
+        The number of samples (or total weight) in a neighborhood for a point
+        to be considered as a core point. This includes the point itself.
+
+    metric : str, default 'cosine'
+        The metric to use when calculating distance between instances in a
+        feature array.
+
+    Returns
+    -------
+    ClusterTransformer
+        Sklearn transformer component that wraps DBSCAN.
+    """
+    model = DBSCAN(eps=eps, min_samples=min_samples, metric=metric)
+    return ClusterTransformer(model, metric="cosine")
+
+
+def KMeansTransformer(n_clusters: int) -> ClusterTransformer:
+    """Convenience function for creating a KMeans transformer.
+
+    Parameters
+    ----------
+    n_clusters: int
+        Number of clusters.
+
+    Returns
+    -------
+    ClusterTransformer
+        Sklearn transformer component that wraps KMeans.
+    """
+    model = KMeans(n_clusters=n_clusters)
+    return ClusterTransformer(model, metric="cosine")
+
+
+def SpectralClusteringTransformer(n_clusters: int) -> ClusterTransformer:
+    """Convenience function for creating a Spectral Clustering transformer.
+
+    Parameters
+    ----------
+    n_clusters: int
+        Number of clusters.
+
+    Returns
+    -------
+    ClusterTransformer
+        Sklearn transformer component that wraps SpectralClustering.
+    """
+    model = SpectralClustering(n_clusters=n_clusters)
+    return ClusterTransformer(model, metric="cosine")
--- blackbert/mixture.py
+++ blackbert/mixture.py
@@ -1,13 +1,107 @@
+from typing import Optional
+
 from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.mixture import BayesianGaussianMixture, GaussianMixture
 
 
 class MixtureTransformer(TransformerMixin):
+    """Turns sklearn mixture models into a transformer component that
+    you can use in a pipeline.
+    This means that the transform() method will return the probability
+    of the mixture components.
+
+    Parameters
+    ----------
+    model: ClusterMixin
+        Sklearn mixture model.
+    """
+
     def __init__(self, model: BaseEstimator):
         self.model = model
 
     def fit(self, X, y=None):
+        """Fits the mixture model.
+
+        Parameters
+        ----------
+        X: ndarray of shape (n_observations, n_features)
+            Observations to cluster.
+        y: None
+            Ignored, exists for compatiblity.
+
+        Returns
+        -------
+        self
+        """
         self.model.fit(X)
         return self
 
     def transform(self, X):
+        """Returns probabilities of mixture components.
+
+        Parameters
+        ----------
+        X: ndarray of shape (n_observations, n_features)
+            Observations to cluster.
+
+        Returns
+        -------
+        ndarray of shape (n_observations, n_clusters)
+            Probabilities of mixture components for all
+            observations.
+        """
         return self.model.predict_proba(X)
+
+
+def NormalMixture(n_components: int) -> MixtureTransformer:
+    """Convenience function for using a Gaussian Mixture model
+    for finding topics. Wraps the GaussianMixture class from sklearn,
+    thus uses the Expectation Minimization algorithm.
+
+    Parameters
+    ----------
+    n_components: int
+        Number of mixture components.
+
+    Returns
+    -------
+    MixtureTransformer
+        Sklearn transforer wrapping GaussianMixture.
+    """
+    model = GaussianMixture(n_components=n_components)
+    return MixtureTransformer(model)
+
+
+def DirichletNormalMixture(
+    n_components: int, concentration: Optional[float] = None
+) -> MixtureTransformer:
+    """Convenience function creating a Gaussian Mixture Model
+    for estimating topic components. Uses variational inference and sets
+    a Dirichlet prior over cluster weights.
+    This means that this model can easily be used for empirically
+    determining the number of topics as the weight
+    of unimportant components vanishes.
+
+    Parameters
+    ----------
+    n_components: int
+        Number of mixture components. It is advisable to set this to
+        a relatively high number so that the number of topics can
+        be inferred from the data.
+    concentration: float or None, default None
+        Concentration parameter of the Dirichlet prior on the component
+        weights.
+        If you want the topics to be more concentrated, then set this to
+        a lower number. If tou want to have more or less equally sized topics
+        set this to something higher.
+        Default value is 1/n_components.
+
+    Returns
+    -------
+    MixtureTransformer
+        Sklearn transformer wrapping BayesianGaussianMixture.
+    """
+    model = BayesianGaussianMixture(
+        n_components=n_components, weight_concentration_prior=concentration
+    )
+    return MixtureTransformer(model)
